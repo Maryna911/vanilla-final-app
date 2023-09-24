076223552d54b23474815e2013669581fe126728
Maryna <141838804+Maryna911@users.noreply.github.com>
2023-09-24 13:44:39 +0300

    Improved the project

--- src/index.js
+++ src/index.js
@@ -75,7 +75,6 @@ function displayForecast(response) {
 
 ///
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "ca0db41e2e878c74a1dfc7ffece370d4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
@@ -115,7 +114,6 @@ function handleSubmit(event) {
   event.preventDefault();
   let cityInputElement = document.querySelector("#city-input");
   search(cityInputElement.value);
-  console.log(cityInputElement.value);
 }
 
 let form = document.querySelector("#search-form");
